222e1111e690e1024cc484a22fd9cd8f9ea95fd4
korgo <87038432+MeloticZ@users.noreply.github.com>
2024-11-24 02:55:03 -0800

    Update some config for demo

--- app/store/config.ts
+++ app/store/config.ts
@@ -73,7 +73,7 @@ export const DEFAULT_CONFIG = {
     frequency_penalty: 0,
     sendMemory: true,
     historyMessageCount: 4,
-    compressMessageLengthThreshold: 1000,
+    compressMessageLengthThreshold: 1638400,
     compressModel: "",
     compressProviderName: "",
     enableInjectSystemPrompts: true,
